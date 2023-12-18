5a31a2f414dfb389e16d378f09ce800e41cb64f1
DESKTOP-UMHL26H\skw42 <skw4223@naver.com>
2023-12-18 20:46:36 +0900

    feat : Login 화면, handle Event

--- presentation/src/main/java/com/side/runwithme/ui/screens/login/LoginScreen.kt
+++ presentation/src/main/java/com/side/runwithme/ui/screens/login/LoginScreen.kt
@@ -1,5 +1,7 @@
 package com.side.runwithme.ui.screens.login
 
+import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -11,15 +13,22 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -37,6 +46,11 @@ import com.side.runwithme.components.RoundedWhiteButton
 import com.side.runwithme.components.VerticalSpacer
 import com.side.runwithme.navigation.BottomNavItem
 import com.side.runwithme.ui.theme.RunWithMeTheme
+import com.side.runwithme.utils.ResponseCodeStatus
+import com.side.runwithme.viewmodel.EventViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @Preview(showBackground = true)
 @Composable
@@ -51,10 +65,20 @@ fun LoginPreview() {
 @Composable
 fun LoginScreen(
     navController: NavController,
-    loginViewModel: LoginViewModel = viewModel()
+    loginViewModel: LoginViewModel = hiltViewModel()
 ) {
 
-    val email = remember {
+    val snackbarHostState = remember { SnackbarHostState() }
+
+    val context = LocalContext.current
+
+    val scope = rememberCoroutineScope()
+
+    val loginSuccessMsg = stringResource(id = R.string.login_success)
+    val loginFailMsg = stringResource(id = R.string.login_fail)
+    val loginErrorMsg = stringResource(id = R.string.login_error)
+
+    val email = rememberSaveable {
         mutableStateOf("")
     }
 
@@ -66,9 +90,39 @@ fun LoginScreen(
         mutableStateOf(false)
     }
 
+    LaunchedEffect(Unit) {
+        loginViewModel.eventFlow.collectLatest {
+            when (it) {
+                is EventViewModel.Event.Success -> {
+                    if (it.code == ResponseCodeStatus.LOGIN_SUCCESS.code) {
+                        Toast.makeText(context, loginSuccessMsg, Toast.LENGTH_SHORT).show()
+                        navController.navigate(BottomNavItem.Home.screenRoute)
+                    } else {
+                        scope.launch {
+                            snackbarHostState.showSnackbar(loginFailMsg)
+                        }
+                    }
+                }
+
+                is EventViewModel.Event.Error -> {
+                    scope.launch {
+                        snackbarHostState.showSnackbar(loginErrorMsg)
+                    }
+                }
+
+                is EventViewModel.Event.Loading -> {
+
+                }
+            }
+        }
+    }
+
     Scaffold(
         modifier = Modifier
-            .fillMaxSize()
+            .fillMaxSize(),
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        }
     ) {
         Box(
             modifier = Modifier
@@ -109,31 +163,40 @@ fun LoginScreen(
 
                 VerticalSpacer(16)
 
-                LoginButton(navController = navController, loginViewModel = loginViewModel)
+                LoginButton {
+                    loginViewModel.loginWithEmail(email = email.value, password = password.value)
+                }
 
                 VerticalSpacer(16)
 
-                JoinButton(navController = navController)
+                JoinButton {
+                    navController.navigate(BottomNavItem.Home.screenRoute)
+                }
             }
         }
     }
 }
 
 @Composable
-fun LoginButton(navController: NavController, loginViewModel: LoginViewModel){
+fun LoginButton(onClick: () -> Unit) {
     RoundedStrongButton(
         stringResource(id = R.string.login)
     ) {
-        navController.navigate(BottomNavItem.Home.screenRoute)
+        onClick.invoke()
     }
 }
 
 @Composable
-fun JoinButton(navController: NavController){
+fun JoinButton(onClick: () -> Unit) {
     RoundedWhiteButton(
         stringResource(id = R.string.join)
     ) {
-        navController.navigate(BottomNavItem.Home.screenRoute)
+        onClick.invoke()
     }
 }
 
+fun showSnackBar(scope: CoroutineScope, snackbarHostState: SnackbarHostState, msg: String) {
+    scope.launch {
+        snackbarHostState.showSnackbar(msg)
+    }
+}
